47094880a5a2ca4f0c3b88a04719b33e0578d9d7
Dmitriy <povorot200@gmail.com>
2022-06-05 18:55:17 +0300

    Закончил стилизацию

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -7,24 +7,41 @@ import Paper from '@mui/material/Paper';
 const App: React.FunctionComponent = () => {
 	return <Box sx={{
 					 backgroundColor: 'rgb(240, 240, 240)',
-					 height: '97vh',
-					 display: 'flex',
-					 //overflow: 'auto'
+					 backgroundImage: 'url("wood.jpg")',
+					 height: '100vh',
+					 overflow: 'auto'
 					}}>
 
+		<h1 style={{
+					margin: 10,
+					paddingTop: 10,
+					textAlign: 'center',
+					fontFamily: 'cursive',
+					fontSize: '40px'
+		}}>
+			Гроссбух
+		</h1>
 		<Box sx={{
-					padding: '20px',
-					margin: '20px',
-					flex: '1',
-					overflow: 'auto'
-				}}
-			component={Paper}
-		>
-            <ProductList />
-			<InTotal />
+			display: 'flex', 
+			alignItems: 'flex-start'
+		}}>
+			<Box sx={{
+						padding: '20px',
+						margin: '20px',
+						flex: '1',
+						height: '75vh',
+						overflow: 'auto',
+					}}
+				component={Paper}
+			>
+            	<ProductList />
+				<InTotal />
+			</Box>
+			<Box>
+				<NewProduct />
+				<img src='wing.png' height='250px' style={{display: 'block', margin: '0 auto'}} />
+			</Box>
 		</Box>
-		<NewProduct />
-
     </Box>;
 };
 
