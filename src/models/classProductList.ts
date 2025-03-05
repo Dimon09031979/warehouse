11ed2f1288915a80f2915078ed805e81448e521f
Dmitriy <povorot14054@gmail.com>
2025-03-05 16:11:03 +0200

    убрал ошибку в консоли

--- src/models/classProductList.ts
+++ src/models/classProductList.ts
@@ -1,4 +1,4 @@
-import { observable, makeObservable, computed } from 'mobx';
+import { observable, makeObservable, computed, action } from 'mobx';
 import type { ProductType } from './types';
 import Product from './classProduct';
 import { fetchData } from '../api';
@@ -26,24 +26,32 @@ class ProductList {
   async initializeList() {
     try {
       const productList: ProductType[] = await fetchData();
-      productList.forEach((product) => {
-        product.sum = product.price * product.weight;
-        product.check = true;
-      });
-      this.list = productList.map(prod => new Product(prod));
+      this.updateList(productList);
     } catch (error) {
-      console.error("Failed to fetch data:", error);
+      console.error("Не удалось загрузить данные:", error);
     }
   }
 
+  @action
+  updateList(productList: ProductType[]) {
+    productList.forEach((product) => {
+      product.sum = product.price * product.weight;
+      product.check = true;
+    });
+    this.list = productList.map(prod => new Product(prod));
+  }
+
+  @action
   invertCheck(i: number) {
     this.list[i].invertCheck();
   }
 
+  @action
   delProduct(i: number) {
     this.list.splice(i, 1);
   }
 
+  @action
   addProduct(newproduct: Product) {
     this.list.push(newproduct);
   }
