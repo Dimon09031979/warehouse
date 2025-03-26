7d1fca8709907c6d139ed23eb1e0f5ff3b213662
Dmitriy <povorot14054@gmail.com>
2025-03-26 11:34:37 +0200

    заменил alert на toast

--- src/models/classProductList.ts
+++ src/models/classProductList.ts
@@ -28,7 +28,7 @@ class ProductList {
       const productList: ProductType[] = await downloadData();
       this.updateList(productList);
     } catch (error) {
-      alert("Не удалось загрузить данные:" + error);
+      toast.error("Не удалось загрузить данные:" + error);
     }
   }
 
