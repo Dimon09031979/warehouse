e1fddc297c7a777872e34b20f0ea5291ca37a8ad
Dmitriy <povorot14054@gmail.com>
2025-10-25 14:50:02 +0300

    use IconButton

--- src/views/Product.tsx
+++ src/views/Product.tsx
@@ -6,6 +6,9 @@ import Checkbox from '@mui/material/Checkbox';
 import TableRow from '@mui/material/TableRow';
 import TableCell from '@mui/material/TableCell';
 import { ProductType } from '../models/classProduct';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import IconButton from '@mui/material/IconButton';
 
 const Product: React.FunctionComponent<{ index: number }> = observer((props) => {
   const index: number = props.index;
@@ -13,6 +16,9 @@ const Product: React.FunctionComponent<{ index: number }> = observer((props) => 
 
   const invertCheck = (i: number) => productList.invertCheck(i);
   const delProduct = (i: number) => productList.delProduct(i);
+
+  const theme = useTheme();
+  const isLgUp = useMediaQuery(theme.breakpoints.up('lg'));
 
   return (
     <TableRow>
@@ -24,15 +30,31 @@ const Product: React.FunctionComponent<{ index: number }> = observer((props) => 
         <Checkbox checked={product.check} onChange={() => invertCheck(index)} />
       </TableCell>
       <TableCell>
-        <Button
-          size='small'
-          variant='outlined'
-          color='secondary'
-          startIcon={<DeleteIcon />}
-          onClick={() => delProduct(index)}
-        >
-          Delete
-        </Button>
+        {isLgUp ? (
+          <Button
+            size='small'
+            variant='outlined'
+            color='secondary'
+            startIcon={<DeleteIcon />}
+            onClick={() => delProduct(index)}
+          >
+            Delete
+          </Button>
+        ) : (
+          <IconButton
+            color='secondary'
+            onClick={() => delProduct(index)}
+            size='small'
+            sx={{
+              border: '1px solid',
+              borderColor: 'secondary.main',
+              borderRadius: '4px',
+              padding: '4px',
+            }}
+          >
+            <DeleteIcon />
+          </IconButton>
+        )}
       </TableCell>
     </TableRow>
   );
